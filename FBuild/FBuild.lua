75e6dce904aa4ae3fa765443d825542919cbaa51
Frank Barwich <frank.barwich@gmail.com>
2012-07-10 14:38:36 +0200

    Build-Script for FBuild.exe now uses FBuild.BuildExe() instead of doing it by hand.

--- FBuild/FBuild.lua
+++ FBuild/FBuild.lua
@@ -7,43 +7,18 @@ if Build ~= "Release" and Build ~= "Debug" then
    error("Build='<Release|Debug>' expected");
 end;
 
-local dep = {
-   Outdir      = Build;  
-   Includes    = { "/boost_1_50_0" };
-   Files       = FBuild.Glob("*.cpp");
-};
-
-local outOfDate = FBuild.CppOutOfDate(dep);
-
-local compileOptions = {
-   Config            = Build;                
-   Outdir            = Build; 
-   Includes          = { "/boost_1_50_0" };
-   CRT               = 'Static';
-   Files             = outOfDate;
-   PrecompiledHeader = {         
-      Header = 'Precompiled.h';  
-      Cpp    = 'Precompiled.cpp';
-   };
-};
-
-
-FBuild.Compile(compileOptions); 
-
-
-local linkOptions = {
-   Config = Build;                
-   Output = "../" .. Build .. "/FBuild.exe";
-   Libpath = { "/boost_1_50_0/stage/lib", "../" .. Build };
-   Libs = { 
-      "Lua.lib",
-      "Shlwapi.lib",
-   };
-   Files = FBuild.Glob(Build, "*.obj");
-   
-};
-
-if FBuild.FileOutOfDate(linkOptions.Output, linkOptions.Files) then
-   FBuild.Link(linkOptions); 
-end                      
+FBuild.BuildExe({
+                   Output = "../" .. Build .. "/FBuild.exe";
+                   Config   = Build;
+                   Outdir   = Build;
+                   Includes = { "/boost_1_50_0" };
+                   CRT      = 'Static';
+                   Files    = FBuild.Glob("*.cpp");
+                   PrecompiledHeader = {         
+                      Header = 'Precompiled.h';  
+                      Cpp    = 'Precompiled.cpp';
+                   };
+                   Libpath = { "/boost_1_50_0/stage/lib", "../" .. Build };
+                   Libs = { "Lua.lib", "Shlwapi.lib", };
+                });
 
